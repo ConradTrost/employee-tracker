888914b154bcc86eca1c661bdeab8ec0e0dbac7d
Conrad Trost <ctrost@tamu.edu>
2020-06-10 17:25:04 -0500

    Added view dept, roles, employees. Add dept, role, employees, update employees (needs fix)

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO departments (name) VALUES ('Tom'), ('Todd'), ('Tim'), ('Jim'), ('Jimbo'), ('Mike');
+INSERT INTO departments (department) VALUES ('Software'), ('Hardware'), ('Human Resources');
 
-INSERT INTO roles (title, salary, department_id) VALUES ('Manager', 100000.90, 2), ('Engineer', 5000, 2);
+INSERT INTO roles (title, salary, department_id) VALUES ('Manager', 100000.90, 2), ('Engineer', 5000, 1), ('Engineer', 10020000, 2);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id) 
 VALUES 
--- server.js
+++ server.js
@@ -16,8 +16,61 @@ connection.connect(err => {
     return;
 })
 
-function viewRoles() {
-    console.log('Nice. Bag Secured...');
+const showEmployeeChoices = () => {
+    const employeeChoices = [];
+    const sql = 'select first_name, last_name, id from employees;';
+    const params = [];
+    connection.query(sql, params, function(err, results) {
+        if(err) throw err;
+        for (n in results) {
+            employeeChoices.push([results[n].id, results[n].first_name, results[n].last_name].join(' '));
+
+        } showRoleChoices(employeeChoices);
+    });
+};
+
+const showRoleChoices = employeeChoices => {
+    const roleChoices = [];
+    const sql = `SELECT id, title FROM roles;`;
+    const params = [];
+    connection.query(sql, params, function(err, results) {
+        if (err) throw err;
+        for (n in results) {
+            roleChoices.push([results[n].id, results[n].title].join(' '));
+        } updateRole(employeeChoices, roleChoices);
+    });
+};
+
+const updateRole = (employeeChoices, roleChoices) => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'Employee',
+            choices: employeeChoices
+        },
+        {
+            type: 'list',
+            name: 'Role',
+            choices: roleChoices
+        }
+    ])
+    .then(answer => {
+        const toFindInt = answer.Employee.split(' ');
+        const indexNumber = toFindInt[0];
+        const toFindRole = answer.Role.split(' ');
+        const indexRole = toFindRole[0];
+        console.log(indexRole);
+        const sql = `UPDATE employees SET manager_id = ${indexRole} WHERE id = ${indexNumber}`;
+        const params = [];
+        connection.query(sql, params, function(err, result) {
+            if(err) throw err;
+            console.table(result);
+            exitMenu();
+        })
+    });
+};
+
+const exitMenu = () => {
     inquirer.prompt([
         {
             type: 'list',
@@ -33,7 +86,18 @@ function viewRoles() {
                 endConnection();
             }
         })
-}
+};
+
+function viewRoles() {
+    const sql = `SELECT roles.id, title, departments.department, salary FROM roles
+    LEFT JOIN departments on departments.id = roles.department_id;`
+    const params = [];
+    connection.query(sql, params, function(err, results) {
+        if(err) throw err;
+        console.table(results);
+        exitMenu();
+    });
+};
 
 function verify(answers) {
     const ans = answers.menuChoice;
@@ -47,20 +111,129 @@ function verify(answers) {
         viewEmployees();
     }
     else if(ans === 'Add a department'){
-        viewRoles();
+        addDepartment();
     }
     else if(ans === 'Add a role'){
-        viewRoles();
+        deptChoices();
     }
     else if(ans === 'Add an employee'){
-        viewRoles();
+        addEmployee();
     }
     else if(ans === 'Update employee role'){
-        viewRoles();
+        showEmployeeChoices();
     }
     else {
         endConnection();
     }
+}
+
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'text',
+            name: 'deptName',
+            message: 'Insert department name:'
+        }
+    ])
+    .then(answer => {
+        const sql = `INSERT INTO departments (department) VALUES (?);`
+        const params = [answer.deptName];
+        connection.query(sql, params, function(err, result) {
+            if(err) throw err;
+            console.table(result);
+            exitMenu();
+        });
+    });
+};
+
+const deptChoices = () => {
+    const sql = `SELECT department FROM departments`;
+    const params = [];
+    const departmentArray = [];
+    connection.query(sql, params, function(err, results) {
+        if (err) throw err;
+        for (n in results) {
+            departmentArray.push(results[n].department);
+        };
+        addRole(departmentArray);
+    });
+};
+
+const addRole = async departmentArray => {
+        inquirer.prompt([
+        {
+            type: 'text',
+            name: 'title',
+            message: 'Insert role name:'
+        },
+        {
+            type: 'number',
+            name: 'salary',
+            message: 'Insert the salary:'
+        },
+        {
+            type: 'list',
+            name: 'dept',
+            choices: departmentArray
+        }
+    ])
+    .then(answers => {
+        // Return the ID of the department
+        console.log(answers.dept);
+        let sql = `SELECT id FROM departments WHERE department = '${answers.dept}';`;
+        connection.query(sql, function(err, results) {
+            if(err) throw err;
+            const deptID = results[0].id;
+            console.log(deptID);
+            addValuesDept(deptID, answers);
+        })
+    });
+};
+
+const addValuesDept = (deptID, answers) => {
+    sql = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?);`;
+    console.log(deptID)
+    const params = [answers.title, answers.salary, deptID]
+    connection.query(sql, params, function(err, results) {
+        if(err) throw err;
+        console.table(results);
+        exitMenu();
+    });
+};
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'text',
+            name: 'firstName',
+            message: 'Insert first name:'
+        },
+        {
+            type: 'text',
+            name: 'lastName',
+            message: 'Insert last name:'
+        },
+        {
+            type: 'number',
+            name: 'roleId', // This needs fix
+            message: 'Insert role id'
+        },
+        {
+            type: 'number',
+            name: 'managerId',
+            message: 'Insert manager ID or leave blank',
+            default: false
+        }
+    ])
+    .then(answers => {
+        const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+        const params = [answers.firstName, answers.lastName, answers.roleId, answers.managerId];
+        connection.query(sql, params, function(err, results) {
+            if(err) throw err;
+            console.table(results);
+            exitMenu();
+        })
+    })
 }
 
 const showMenu = () => {
@@ -83,17 +256,27 @@ const viewDepartments = () => {
     connection.query(sql, params, function(err, results) {
         if(err) throw err;
         console.table(results);
-    })
-}
+        exitMenu();
+    });
+};
 
 const viewEmployees = () => {
     console.log('Showing employees...')
-    const sql = 'select * from employees left join roles on employees.role_id = roles.id;';
+    const sql = `select managers.first_name, managers.last_name, employees.first_name, employees.last_name, title, salary, departments.department from employees 
+    left join roles on employees.role_id = roles.id
+    left join departments on roles.department_id = departments.id
+    left join employees as managers on employees.manager_id = managers.id;`; // Show manager name!!!
     const params = [];
-    
-}
+    connection.query(sql, params, function(err, results) {
+        if(err) throw err;
+        console.table(results);
+        exitMenu();
+    });
+};
 
 const endConnection = () => {
     console.log('Thanks for supporting this app! Bye.')
     connection.end();
-}
+};
+
+//UPDATE EMPLOYEE ROLE NOT WORKING EITHER
